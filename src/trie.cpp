f7d877bdbd0c7f735868d8ac2da05492b5287443
Ali Mosaloo <m.mosalo1382@gmail.com>
2022-11-13 16:50:20 +0330

    copy constructor was build again

--- src/trie.cpp
+++ src/trie.cpp
@@ -3,6 +3,7 @@ void Trie::insert(const std::string& str)
 {
     //first time calling;
     Node*  a {root};
+    
     auto init{root->children.size()};
     if(init==0)
     {
@@ -106,65 +107,73 @@ bool Trie::search(const std::string& query)
 void Trie::bfs(std::function<void(Node*& node)> func)
 {
     Node* _root{root};
-    Node* next{root}; 
     std::vector <Node*> b; 
     
     size_t counter{};
-    b.push_back(next);
+    b.push_back(_root);
     size_t i{} ;
     size_t saver{};
     while((_root=b[counter]))
     {
         for(auto x:_root->children)
         {
-            std::cout<<x->data<<std::endl;
             if(x==nullptr) {break;}
             b.push_back(x);
             
         }
-        std::cout<<"end"<<std::endl;
         counter++;
     }
-       std::cout<<b.size()<<std::endl; 
     for(auto x:b)
     {
         func(x);
     }
     
 }
-Trie::Trie(const Trie& trie)
+Trie::Trie( Trie& trie)
 {
-    size_t counter{};
-    Node * copy{new Node{'\0',false}};
-    Node * a{copy};
-    Node * main{trie.root};
-    std::vector<Node*> copy_vec;
-    std::vector<Node*> vec;
-    for(int i{};i<main->children.size();i++)
-    {
-        copy->children.push_back(new Node{main->children[i]->data,main->children[i]->is_finished});
-    std::cout<<copy->children[i]->data<<std::endl;
-        copy_vec.push_back(copy->children[i]);
-        vec.push_back(main->children[i]);
-        if(main->children[i+1]==nullptr)
-        {
-            while(vec[counter]->is_finished==true)
-            {
-                std::cout<<"end: "<<vec[counter]->data<<std::endl;
-                counter++;
-                if(vec[counter+1]==nullptr)
-                {break;}
-            }
-            copy=copy_vec[counter];
-            main=vec[counter];
-            
-            counter++ ; 
-            i =-1 ; 
-        }
-    } 
-    root =a ;
+    Node* _root{root} ;
+    Node* saver{_root}; 
+    _root =new Node{*trie.root};
+    saver = _root ; 
+    std::cout<<saver<<std::endl;
+    std::cout<<_root<<std::endl;
 
+
+
+
+
+    int counter{} ;
+    int i{} ; 
+    Node* __root{root} ; 
+    __root = new Node{trie.root->data,trie.root->is_finished};
+    root = __root; 
+    std::cout<<"first"<<root->is_finished<<std::endl;
+    std::vector<Node*> main ; 
+    std::vector<Node*> copy ; 
+    main.push_back(trie.root);
+
+    copy.push_back(root);
+    while((__root=main[counter]))
+    {
+        for(auto x:__root->children)
+        {
+            if(x==nullptr) {break;}
+            __root->children[i] = new Node{*x};
+
+            std::cout<<"i: "<<i<<" "<<__root->children[i]->data<<std::endl;
+            main.push_back(x); 
+            copy.push_back(x);
+            i++;
+        }
+        i = 0;
+        counter++;
+        __root = copy[counter];
+    }    
+    root =saver ;
 }
+
+
+
 
 
 
